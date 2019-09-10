dbdb438f1117141ce3f2317d20272ec67a88c730
Laura Jiang <jianglau@oregonstate.edu>
2019-09-09 23:27:32 -0700

    sep 9

--- leetcode/easy/202.happy-number.py
+++ leetcode/easy/202.happy-number.py
@@ -5,15 +5,11 @@
 #
 class Solution:
     def isHappy(self, n: int) -> bool:
-        d = {}
-        while True:
-            nn = str(n)
-            num = sum([int(x) ** 2 for x in nn])
-        if num in d:
-            return False
-        else:
-            d[num] = 1
-            if num == 1:
-                return True
-            n = num
+        s = set()
+        while n != 1:
+            if n in s:
+                return False
+            s.add(n)
+            n = sum([int(i) ** 2 for i in str(n)])
+        return True
 
