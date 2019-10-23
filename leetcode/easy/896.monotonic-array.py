403c56d968cc83c4e5249b9c897328288ad34186
Laura Jiang <jianglau@oregonstate.edu>
2019-10-23 09:32:10 -0700

    oct 23

---
+++ leetcode/easy/896.monotonic-array.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=896 lang=python3
+#
+# [896] Monotonic Array
+#
+
+# @lc code=start
+class Solution:
+    def isMonotonic(self, A: List[int]) -> bool:
+        
+# @lc code=end
+
