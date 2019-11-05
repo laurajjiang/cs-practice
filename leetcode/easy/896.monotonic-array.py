0c53531d1f9691322f28c00e4170c31fb362be8e
Laura Jiang <jianglau@oregonstate.edu>
2019-11-04 20:14:34 -0800

    nov 4

--- leetcode/easy/896.monotonic-array.py
+++ leetcode/easy/896.monotonic-array.py
@@ -7,6 +7,20 @@
 # @lc code=start
 class Solution:
     def isMonotonic(self, A: List[int]) -> bool:
-        
+        return self.isMonoInc(A) or self.isMonoDec(A)
+
+    def isMonoInc(self, A: List[int]) -> bool:
+        for i in range(len(A) - 1):
+            if A[i] > A[i + 1]:
+                return False
+        return True
+
+    def isMonoDec(self, A: List[int]) -> bool:
+        for i in range(len(A) - 1):
+            if A[i] < A[i + 1]:
+                return False
+        return True
+
+
 # @lc code=end
 
