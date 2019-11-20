44b0c03e580ec6f5cd7b04bf443c4cd6d3345eb1
Laura Jiang <jianglau@oregonstate.edu>
2019-11-19 19:35:32 -0800

    nov 19, binary search tree work

--- leetcode/easy/257.binary-tree-paths.py
+++ leetcode/easy/257.binary-tree-paths.py
@@ -12,8 +12,23 @@
 #         self.left = None
 #         self.right = None
 
+
 class Solution:
     def binaryTreePaths(self, root: TreeNode) -> List[str]:
-        
+        res = []
+        self.dfs(root, "", res)
+        return res
+
+    def dfs(self, root, path, res):
+        if root is None:
+            return
+        if not root.left and not root.right:
+            res.append(path + str(root.val))
+        if root.left:
+            self.dfs(root.left, path + str(root.val) + "->", res)
+        if root.right:
+            self.dfs(root.right, path + str(root.val) + "->", res)
+
+
 # @lc code=end
 
