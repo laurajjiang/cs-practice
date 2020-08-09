09c948fe3efe958a48320f6188a1a412ea79a4a4
Laura Jiang <jianglau@oregonstate.edu>
2020-08-08 22:44:14 -0700

    dyn. programming first

---
+++ leetcode/recursion/119.pascaltriangle2.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        ans = [1 for _ in range(rowIndex+1)]
+        
+        for i in range(1, rowIndex):
+            for j in range(i, 0, -1):
+                ans[j] += ans[j-1]
+        return ans
