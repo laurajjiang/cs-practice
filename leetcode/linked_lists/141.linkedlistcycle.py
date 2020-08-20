e7da7d0ce9f48508d4dea11acc0454985cbe0e55
Laura Jiang <jianglau@oregonstate.edu>
2020-08-19 18:37:54 -0700

    ll

---
+++ leetcode/linked_lists/141.linkedlistcycle.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if head is None or head.next is None:
+            return False
+
+        slow = head
+        fast = head.next
+        while fast != slow:
+            if fast is None or fast.next is None:
+                return False
+            fast = fast.next.next
+            slow = slow.next
+        return True
